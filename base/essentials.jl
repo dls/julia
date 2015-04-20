ff582f8b8342743f554cd7b1ff6a796c977bc96f
Jeff Bezanson <jeff.bezanson@gmail.com>
2015-04-20 00:32:44 -0400

    merge Union and UnionType; allow writing union types with `Union{ }`

--- base/essentials.jl
+++ base/essentials.jl
@@ -1,5 +1,8 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
+# TODO: deprecate
+call(::Type{Union}, args...) = Union{args...}
+
 abstract IO
 
 typealias Callable Union(Function,DataType)
